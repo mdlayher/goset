f71df813498ce1c99d3eef509edafcad0d212277
Matt Layher <mdlayher@gmail.com>
2014-04-02 21:07:40 -0400

    Initial commit of set.go and set_test.go

---
+++ set.go
@@ -0,0 +1,71 @@
+package set
+
+import (
+	"sync"
+)
+
+// Set represents an unordered collection of unique values
+type Set struct {
+	// Mutex to allow safe, concurrent access
+	mutex sync.RWMutex
+	// Empty struct consumes no memory, so we just use the map keys
+	m map[interface{}]struct{}
+}
+
+// New creates a new Set, and initializes its internal map, optionally adding
+// initial elements to the set
+func New(values ...interface{}) *Set {
+	// Initialize set
+	s := Set{
+		m: make(map[interface{}]struct{}),
+	}
+
+	// If items are specified in the initializer, immediately add them to the set
+	for _, v := range values {
+		s.Add(v)
+	}
+
+	return &s
+}
+
+// Add inserts a new element into the set, returning true if the element was newly
+// added, or false if it already existed
+func (s *Set) Add(value interface{}) bool {
+	// Check existence
+	found := s.Has(value)
+
+	// Lock set for write
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	// Add value to set
+	s.m[value] = struct{}{}
+
+	// Return inverse, so true if element already existed
+	return !found
+}
+
+// Has checks for membership of an element in the set
+func (s *Set) Has(value interface{}) bool {
+	// Lock set for read
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	// Check for value
+	if _, ok := s.m[value]; ok {
+		// Value found
+		return true
+	}
+
+	// Value not found
+	return false
+}
+
+// Size returns the size or cardinality of this set
+func (s *Set) Size() int {
+	// Lock set for read
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	return len(s.m)
+}
---
+++ set_test.go
@@ -0,0 +1,106 @@
+package set
+
+import (
+	"log"
+	"testing"
+)
+
+// TestAdd verifies that the set.Add() method is working properly
+func TestAdd(t *testing.T) {
+	log.Println("TestAdd()")
+
+	// Create a set, add some initial values
+	set := New(1, 3, 5)
+
+	// Create a table of tests and expected results for adding new elements
+	var tests = []struct {
+		element interface{}
+		result  bool
+	}{
+		// New items
+		{2, true},
+		{4, true},
+		{6, true},
+		// Existing items
+		{1, false},
+		{3, false},
+		{5, false},
+	}
+
+	// Iterate test table, checking results
+	for _, test := range tests {
+		// Attempt to add an element to the set, verify result
+		if ok := set.Add(test.element); ok != test.result {
+			t.Fatalf("set.Add(%d) - unexpected result: %t", test.element, ok)
+		}
+	}
+}
+
+// TestHas verifies that the set.Has() method is working properly
+func TestHas(t *testing.T) {
+	log.Println("TestHas()")
+
+	// Create a set, add some initial values
+	set := New(1, 3, 5)
+
+	// Create a table of tests and expected results for checking membership of elements
+	var tests = []struct {
+		element interface{}
+		result  bool
+	}{
+		// Existing items
+		{1, true},
+		{3, true},
+		{5, true},
+		// Non-existant items
+		{2, false},
+		{4, false},
+		{6, false},
+	}
+
+	// Iterate test table, checking results
+	for _, test := range tests {
+		// Attempt to check if the element is contained in the set, verify result
+		if ok := set.Has(test.element); ok != test.result {
+			t.Fatalf("set.Has(%d) - unexpected result: %t", test.element, ok)
+		}
+	}
+}
+
+// TestSize verifies that the set.Size() method is working properly
+func TestSize(t *testing.T) {
+	log.Println("TestSize()")
+
+	// Create a set, add some initial values
+	set := New(1, 3, 5)
+
+	// Verify initial size
+	if set.Size() != 3 {
+		t.Fatalf("set.Size() - unexpected result: %d", set.Size())
+	}
+
+	// Create a table of tests and expected size when adding new elements
+	var tests = []struct {
+		element interface{}
+		size    int
+	}{
+		// New items
+		{2, 4},
+		{4, 5},
+		{6, 6},
+		// Existing items
+		{1, 6},
+		{3, 6},
+		{5, 6},
+	}
+
+	// Iterate test table, checking results
+	for _, test := range tests {
+		// Add an element to the set, check size
+		set.Add(test.element)
+
+		if set.Size() != test.size {
+			t.Fatalf("set.Size()- unexpected result: %d", set.Size())
+		}
+	}
+}
