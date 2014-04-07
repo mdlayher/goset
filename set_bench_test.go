c21945cbc2ed0b993b1db071c02623812094465d
Matt Layher <mdlayher@gmail.com>
2014-04-07 03:09:26 -0400

    Add BenchmarkCartesianProduct(), small and large variants

--- set_bench_test.go
+++ set_bench_test.go
@@ -14,3 +14,23 @@ func BenchmarkAdd(b *testing.B) {
 		set.Add(i)
 	}
 }
+
+// benchmarkCartesianProduct checks the performance of the set.CartesianProduct() method
+func benchmarkCartesianProduct(n int, s *Set, t *Set) {
+	// Run set.CartesianProduct() n times
+	for i := 0; i < n; i++ {
+		s.CartesianProduct(t)
+	}
+}
+
+// BenchmarkCartesianProductSmall checks the performance of the set.CartesianProduct() method
+// over a small data set
+func BenchmarkCartesianProductSmall(b *testing.B) {
+	benchmarkCartesianProduct(b.N, New(1, 2), New(2, 1))
+}
+
+// BenchmarkCartesianProductLarge checks the performance of the set.CartesianProduct() method
+// over a large data set
+func BenchmarkCartesianProductLarge(b *testing.B) {
+	benchmarkCartesianProduct(b.N, New(1, 2, 3, 4, 5, 6, 7, 8, 9), New(9, 8, 7, 6, 5, 4, 3, 2, 1))
+}
